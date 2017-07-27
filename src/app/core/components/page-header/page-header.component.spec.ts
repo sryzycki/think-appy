7b2b11cf1ac13dd8eb8be9fd4175189bf37ab5c6
Samuel Ryzycki <samuel@ryzycki.com>
2017-07-27 18:14:59 +0100

    test(gratitude-diary): cover page header

--- src/app/core/components/page-header/page-header.component.spec.ts
+++ src/app/core/components/page-header/page-header.component.spec.ts
@@ -1,25 +1,41 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageHeaderComponent } from './page-header.component';
 
+@Component({
+  selector: 'app-test',
+  template: `
+    <app-page-header>
+      Projected!
+    </app-page-header>
+  `,
+})
+class TestComponent {}
+
 describe('PageHeaderComponent', () => {
-  let component: PageHeaderComponent;
+  let component: TestComponent;
   let fixture: ComponentFixture<PageHeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PageHeaderComponent ]
+      declarations: [ TestComponent, PageHeaderComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PageHeaderComponent);
+    fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should project the text', () => {
+    expect(fixture.nativeElement.textContent).toContain('Projected!');
   });
 });
