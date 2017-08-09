04420bf85fca4c13a9c3e853265430dfedd3ed6e
Samuel Ryzycki <samuel@ryzycki.com>
2017-08-09 21:10:11 +0100

    test(gratitude-diary): cover page not found

--- src/app/page-not-found.component.spec.ts
+++ src/app/page-not-found.component.spec.ts
@@ -1,14 +1,21 @@
+import { By } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { CoreModule } from './core/core.module';
 
 import { PageNotFoundComponent } from './page-not-found.component';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PageNotFoundComponent ]
+      declarations: [ PageNotFoundComponent ],
+      imports: [ CoreModule ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
@@ -16,10 +23,17 @@ describe('PageNotFoundComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have "wtf" copy', () => {
+    const copy: string = el.query(By.css('md-card')).nativeElement.textContent;
+
+    expect(copy).toContain('Where were we');
   });
 });
